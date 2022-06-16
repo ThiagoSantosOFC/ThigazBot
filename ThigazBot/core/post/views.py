78938a09cc91c2c697d58dbf4680a15bfacac707
Thiago <74683757+ThiagoShow@users.noreply.github.com>
2022-06-16 17:25:44 +0200

    Stable build

--- ThigazBot/core/post/views.py
+++ ThigazBot/core/post/views.py
@@ -1,3 +1,10 @@
+from modulefinder import IMPORT_NAME
+
+
+import tweepy 
+
+from tweepy.auth import OAuthHandler
+from django.conf import settings
 from django.shortcuts import redirect, render
 
 # Create your views here.
@@ -8,6 +15,13 @@ def index (request):
         
         if content:
             print("Content:", content)
+            
+            auth = tweepy.OAuthHandler(settings.API_KEY, settings.API_KEY_SECRET)
+            auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
+            
+            api = tweepy.API(auth)
+            api.update_status(content)
+            
             
             return redirect ('index')
         
